e65937a82be4a5a362d72130e0bbc8b406f5a106
godunovalexgoal-qa <godunovalexgoal@gmail.com>
2026-04-28 23:37:51 +0400

    gims

---
+++ src/test/java/ru/bulgakov/webshop/test/GimsTest.java
@@ -0,0 +1,42 @@
+package ru.bulgakov.webshop.test;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Condition.visible;
+
+public class GimsTest {
+    @Test
+    @DisplayName("Тест прогона до нужного вопроса")
+    @Tags({@Tag("UI"), @Tag("positive")})
+    void studyGimsTest() {
+//        Configuration.browser = "chrome"; // выбор браузера
+//        Configuration.holdBrowserOpen = true;
+        String numberList = "538";
+
+        open("https://digital.mchs.gov.ru/gims/simulator");
+        $$(".form-check-label").last().click();
+        $("#gims_simulator_form_send").shouldBe(visible).click();
+        sleep(3000);
+        $$(".answers-label").last().click();
+        $(".button-step").shouldBe(visible).click();
+        $(".button-step").shouldBe(visible).click();
+        while (!$("[data-name='question-number']").shouldBe(visible).getText().equals(numberList)) {
+            sleep(3000);
+            $$(".answers-label").last().click();
+            $(".button-step").shouldBe(visible).click();
+            $(".button-step").shouldBe(visible).click();
+        }
+        $("[data-name='question-number']").shouldHave(text(numberList));
+
+        System.out.println("Текущий URL: " + WebDriverRunner.url());
+
+    }
+}
