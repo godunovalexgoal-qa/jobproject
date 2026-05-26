9adeb280fd13bfeaa03a33fccf9cb4b24a27868d
godunovalexgoal-qa <godunovalexgoal@gmail.com>
2026-05-26 18:18:19 +0400

    gims3.1

--- src/test/java/ru/bulgakov/webshop/test/Gims.java
+++ src/test/java/ru/bulgakov/webshop/test/Gims.java
@@ -19,19 +19,19 @@ public class Gims extends TestBase {
     void studyGimsTest() {
 //        Configuration.browser = "chrome"; // выбор браузера
 //        Configuration.holdBrowserOpen = true;
-        String numberList = System.getProperty("QUESTION_NUMBER", "538");
+        String numberList = System.getProperty("QUESTION_NUMBER", "8");
 //        String numberList = "100";
 
         open("https://digital.mchs.gov.ru/gims/simulator");
         $$(".form-check-label").last().click();
         $("#gims_simulator_form_send").shouldBe(visible).click();
         sleep(3000);
-        $$(".answers-label").last().click();
+        $$(".answers-label").first().click();
         $(".button-step").shouldBe(visible).click();
         $(".button-step").shouldBe(visible).click();
         while (!$("[data-name='question-number']").shouldBe(visible).getText().equals(numberList)) {
             sleep(3000);
-            $$(".answers-label").last().click();
+            $$(".answers-label").first().click();
             $(".button-step").shouldBe(visible).click();
             $(".button-step").shouldBe(visible).click();
         }
