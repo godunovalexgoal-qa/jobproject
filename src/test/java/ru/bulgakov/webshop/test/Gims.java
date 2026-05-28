7bf12b9a20dad87ace7f6be29299f8075ef77776
godunovalexgoal-qa <godunovalexgoal@gmail.com>
2026-05-28 12:32:27 +0400

    gims3.4

--- src/test/java/ru/bulgakov/webshop/test/Gims.java
+++ src/test/java/ru/bulgakov/webshop/test/Gims.java
@@ -1,10 +1,8 @@
 package ru.bulgakov.webshop.test;
 
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Tags;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import ru.bulgakov.webshop.TestBase;
 
 import static com.codeborne.selenide.Condition.*;
@@ -13,6 +11,32 @@ import static com.codeborne.selenide.Selenide.$;
 import static java.time.Duration.ofSeconds;
 
 public class Gims extends TestBase {
+
+    @AfterEach
+    void tearDown(TestInfo testInfo) {
+        String testName = testInfo.getDisplayName();
+
+        System.out.println("\n" + "=".repeat(60));
+        System.out.println("ЗАВЕРШЕНИЕ ТЕСТА: " + testName);
+        System.out.println("=".repeat(60));
+
+        try {
+            // Всегда выводим URL
+            String currentUrl = WebDriverRunner.url();
+            System.out.println("✓ Текущий URL: " + currentUrl);
+
+            // Делаем скриншот (даже если тест прошёл - для отладки)
+            String screenshot = Selenide.screenshot("test_" + testName.replaceAll("[^a-zA-Z0-9]", "_"));
+            System.out.println("✓ Скриншот сохранён: " + screenshot);
+
+        } catch (Exception e) {
+            System.err.println("✗ Ошибка при получении информации: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        System.out.println("=".repeat(60) + "\n");
+    }
+
     @Test
     @DisplayName("Тест прогона до нужного вопроса")
     @Tags({@Tag("UI"), @Tag("positive")})
@@ -21,6 +45,8 @@ public class Gims extends TestBase {
 //        Configuration.holdBrowserOpen = true;
         String numberList = System.getProperty("QUESTION_NUMBER", "8");
 //        String numberList = "100";
+
+        System.out.println(">>> Начало теста. Целевой вопрос: " + numberList);
 
         open("https://digital.mchs.gov.ru/gims/simulator");
         $$(".form-check-label").last().click();
@@ -37,7 +63,6 @@ public class Gims extends TestBase {
         }
         $("[data-name='question-number']").shouldHave(text(numberList));
 
-        System.out.println("Текущий URL: " + WebDriverRunner.url());
-
+        System.out.println(">>> Тест успешно завершён!");
     }
 }
